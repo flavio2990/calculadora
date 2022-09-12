46b860b30e65ae4b0c71ef92f7cbc246bb913ac0
flavio2990 <tosi.flaviol@gmail.com>
2022-09-12 00:21:23 -0300

    buttoms values

--- App.js
+++ App.js
@@ -7,41 +7,43 @@ import  Buttons  from './Components/Buttons';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
-      <Buttons />
+      <Buttons value= '1' />
+      <Buttons value= '2' />
+      <Buttons value= '3'/>
+      <Buttons value= '4'/>
+      <Buttons value= '5'/>
+      <Buttons value= '6'/>
+      <Buttons value= '7'/>
+      <Buttons value= '8'/>
+      <Buttons value= '9'/>
+      <Buttons value= '0'/>
+      <Buttons value= '-'/>
+      <Buttons value= '+'/>
+      <Buttons value= 'x'/>
+      <Buttons value= '/'/>
+      <Buttons value= '='/>
+      <Buttons value= 'C'/>
+      <Buttons value= 'D'/>
+      <Buttons value= '.'/>
       <StatusBar style="auto" />
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
+    backgroundColor: 'gray',
     alignItems: 'center',
-    justifyContent: 'space-between',
+    justifyContent: 'center',
     marginHorizontal: 16,
     marginVertical: 20,
     width: 450,
     flexDirection: 'row',
     flexWrap: 'wrap',
+    paddingVertical: 90,
+    paddingHorizontal: "35%",
+    
   },
   
 });

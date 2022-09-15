72dcaaec64241e839bd634d3c43b25cb9be9b8ac
flavio2990 <tosi.flaviol@gmail.com>
2022-09-15 17:58:46 -0300

    display

--- Components/Display/index.js
+++ Components/Display/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { View, Text } from 'react-native';
 
 
-const Display= (props) => {
-    return (<View style ={{width: "100%", height: 80,
-  backgroundColor: 'grey',}}><Text style ={{color:"red"}}>{props.value}</ Text></View>)
+const Display = (props) => {
+  return (<View style={{
+    width: "100%", height: 80,
+    backgroundColor: 'grey',
+  }}><Text style={{ color: "red" }}>{props.value}</ Text></View>)
 }
 
 export default Display
